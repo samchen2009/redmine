9b1e402f6656ef6b7f61a1fe7f610f79d121d2b9
samchen2009 <samchen2009@gmail.com>
2013-01-14 19:37:41 +0800

    remove debugger and support filters

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -53,7 +53,6 @@ class IssuesController < ApplicationController
   include Redmine::Export::PDF
 
   def index
-    debugger
     retrieve_query
     sort_init(@query.sort_criteria.empty? ? [['id', 'desc']] : @query.sort_criteria)
     sort_update(@query.sortable_columns)
