6b9e926bff984ee2d36313a4833320b05a5f4b48
samchen2009 <samchen2009@gmail.com>
2013-01-14 17:18:20 +0800

    add kanban card list

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -53,6 +53,7 @@ class IssuesController < ApplicationController
   include Redmine::Export::PDF
 
   def index
+    debugger
     retrieve_query
     sort_init(@query.sort_criteria.empty? ? [['id', 'desc']] : @query.sort_criteria)
     sort_update(@query.sortable_columns)
